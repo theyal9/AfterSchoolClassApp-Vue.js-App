c42e306ba56d14ea1ac6bafcafdfab4a016f69cd
theyal9 <tdookhy0904@gmail.com>
2024-10-08 20:07:33 +0400

    Modified Checkout page Added multiple posts on Home

---
+++ lessons.js
@@ -0,0 +1,182 @@
+let lessons = [
+    {
+        id: 1,
+        subject: 'Maths',
+        location: 'London',
+        price: 1500,
+        image: 'Assets/images/Maths.jpg',
+        spaces: 5,
+        rating: 3
+    },
+    {
+        id: 2,
+        subject: 'English',
+        location: 'Manchester',
+        price: 1200,
+        image: 'Assets/images/English.jpg',
+        spaces: 5,
+        rating: 4
+    },
+    {
+        id: 3,
+        subject: 'Science',
+        location: 'Liverpool',
+        price: 1800,
+        image: 'Assets/images/Science.jpg',
+        spaces: 5,
+        rating: 5
+    },
+    {
+        id: 4,
+        subject: 'History',
+        location: 'Bristol',
+        price: 1300,
+        image: 'Assets/images/History.jpg',
+        spaces: 5,
+        rating: 4
+    },
+    {
+        id: 5,
+        subject: 'Geography',
+        location: 'Edinburgh',
+        price: 1600,
+        image: 'Assets/images/Geography.jpg',
+        spaces: 5,
+        rating: 3
+    },
+    {
+        id: 6,
+        subject: 'Physics',
+        location: 'Glasgow',
+        price: 2000,
+        image: 'Assets/images/Physics.jpg',
+        spaces: 5,
+        rating: 5
+    },
+    {
+        id: 7,
+        subject: 'Chemistry',
+        location: 'London',
+        price: 1900,
+        image: 'Assets/images/Chemistry.jpg',
+        spaces: 5,
+        rating: 4
+    },
+    {
+        id: 8,
+        subject: 'Biology',
+        location: 'Manchester',
+        price: 1750,
+        image: 'Assets/images/Biology.jpg',
+        spaces: 5,
+        rating: 5
+    },
+    {
+        id: 9,
+        subject: 'Art',
+        location: 'Birmingham',
+        price: 1100,
+        image: 'Assets/images/Art.jpg',
+        spaces: 5,
+        rating: 4
+    },
+    {
+        id: 10,
+        subject: 'Music',
+        location: 'Leeds',
+        price: 1400,
+        image: 'Assets/images/Music.jpg',
+        spaces: 5,
+        rating: 5
+    },
+    {
+        id: 11,
+        subject: 'Physical Education',
+        location: 'Newcastle',
+        price: 1000,
+        image: 'Assets/images/PE.jpg',
+        spaces: 5,
+        rating: 3
+    },
+    {
+        id: 12,
+        subject: 'Computer Science',
+        location: 'Sheffield',
+        price: 1700,
+        image: 'Assets/images/CS.jpg',
+        spaces: 5,
+        rating: 4
+    },
+    {
+        id: 13,
+        subject: 'Drama',
+        location: 'Oxford',
+        price: 1150,
+        image: 'Assets/images/Drama.jpg',
+        spaces: 5,
+        rating: 4
+    },
+    {
+        id: 14,
+        subject: 'Economics',
+        location: 'Cambridge',
+        price: 1650,
+        image: 'Assets/images/Economics.jpg',
+        spaces: 5,
+        rating: 5
+    },
+    {
+        id: 15,
+        subject: 'Psychology',
+        location: 'Brighton',
+        price: 1550,
+        image: 'Assets/images/Psychology.jpg',
+        spaces: 5,
+        rating: 4
+    },
+    {
+        id: 16,
+        subject: 'Philosophy',
+        location: 'York',
+        price: 1450,
+        image: 'Assets/images/Philosophy.jpg',
+        spaces: 5,
+        rating: 3
+    },
+    {
+        id: 17,
+        subject: 'French',
+        location: 'London',
+        price: 1350,
+        image: 'Assets/images/French.jpg',
+        spaces: 5,
+        rating: 4
+    },
+    {
+        id: 18,
+        subject: 'Spanish',
+        location: 'Bristol',
+        price: 1250,
+        image: 'Assets/images/Spanish.jpg',
+        spaces: 5,
+        rating: 5
+    },
+    {
+        id: 19,
+        subject: 'German',
+        location: 'Liverpool',
+        price: 1150,
+        image: 'Assets/images/German.jpg',
+        spaces: 5,
+        rating: 3
+    },
+    {
+        id: 20,
+        subject: 'Sociology',
+        location: 'Manchester',
+        price: 1400,
+        image: 'Assets/images/Sociology.jpg',
+        spaces: 5,
+        rating: 4
+    }
+];
